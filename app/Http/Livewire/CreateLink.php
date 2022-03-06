d8105a271c402284b94ac174b393d61fcc6847d7
Arthur Felipe Kraemer <arthurkx2@hotmail.com>
2022-03-06 18:39:37 -0300

    Adiciona: livewire da rota de criação

--- app/Http/Livewire/CreateLink.php
+++ app/Http/Livewire/CreateLink.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class CreateLink extends Component
 {
+
+    public $slug;
+    public $url;
+
+    protected $rules = [
+        'url' => 'require|url|max:225',
+        'slug' => 'nullable|alpha_dash|unique:links,slug|min:3|max:100',
+    ]
+
     public function render()
     {
         return view('livewire.create-link');
